be5919e54a46e4f917436f1f3e1c83da42b0720e
Tên ManhCuong <cuongbmce171488@fpt.edu.vn>
2025-03-20 07:55:43 +0700

    update

--- src/main/java/edu/fpt/customflorist/repositories/UserRepository.java
+++ src/main/java/edu/fpt/customflorist/repositories/UserRepository.java
@@ -21,13 +21,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE " +
             "((:keyword IS NULL OR LOWER(u.name) LIKE LOWER(CONCAT('%', :keyword, '%'))) OR " +
             "(:keyword IS NULL OR LOWER(u.email) LIKE LOWER(CONCAT('%', :keyword, '%'))) OR " +
-            "(:keyword IS NULL OR LOWER(u.address) LIKE LOWER(CONCAT('%', :keyword, '%')))) " +
-            "AND (:role IS NULL OR u.role = CASE WHEN :role = 'ADMIN' THEN 'ADMIN' WHEN :role = 'CUSTOMER' THEN 'CUSTOMER' WHEN :role = 'SHIPPER' THEN 'SHIPPER' WHEN :role = 'MANAGER' THEN 'MANAGER' ELSE u.role END) " +
-            "AND (:accountStatus IS NULL OR u.accountStatus = CASE WHEN :accountStatus = 'ACTIVE' THEN 'ACTIVE' WHEN :accountStatus = 'BANNED' THEN 'BANNED' ELSE u.accountStatus END) " +
-            "AND (:gender IS NULL OR u.gender = CASE WHEN :gender = 'Male' THEN 'Male' WHEN :gender = 'Female' THEN 'Female' WHEN :gender = 'Other' THEN 'Other' ELSE u.gender END)")
+            "(:keyword IS NULL OR LOWER(u.address) LIKE LOWER(CONCAT('%', :keyword, '%')))) " )
     Page<User> searchUsers(@Param("keyword") String keyword,
                            @Param("role") String role,
                            @Param("accountStatus") String accountStatus,
                            @Param("gender") String gender,
                            Pageable pageable);
+
+
 }
